525145a2e89444732dea39a40f577292972a6e9a
kluan01 <l.kaden.m@gmail.com>
2024-11-11 22:03:41 -0500

    Add Page-rank Algorithm Implementation

--- src/AdjacencyList.cpp
+++ src/AdjacencyList.cpp
@@ -1,16 +1,116 @@
 #include <iostream>
 #include "AdjacencyList.h"
+#include <cmath>
+#include <iomanip>
 
 using namespace std;
 
-// prints the PageRank of all pages after p powerIterations in ascending
-// alphabetical order of webpages and rounding rank to two decimal places
-string AdjacencyList::PageRank(int n){
-    // optionally, store your output in a string/stringstream and then return it from this function after printing so that it is easier to test with Catch
+int AdjacencyList::getOutDegree(const string& website) {
+    int outDegree = 0;
+    // iterate through each contributor website to see if 'website' is a contributor
+    for(auto &it : AdjList) {
+        for(int i = 0; i < it.second.size(); i++) {
+           if(it.second[i].first == website) {
+               outDegree += 1;
+           }
+        }
+    }
+    return outDegree;
+}
+
+void AdjacencyList::insertEdge(const string& from, const string& to, set<string>& websites) {
+    // keep track of number of websites
+    websites.insert(from);
+    websites.insert(to);
+
+    // get out-degrees
+    int outDegree = getOutDegree(from) + 1;
+    // update the out-degree values for current existing values
+    for (auto& website : AdjList) {
+        for(auto & pair : website.second) {
+            if (pair.first == from) {
+                pair.second = 1.0 / outDegree;
+            }
+        }
+    }
+    // add new value to contributing websites
+    AdjList[to].emplace_back(from, 1.0 / outDegree);
+}
+
+void AdjacencyList::insertEmptyNode(const string& website) {
+    if (AdjList.find(website) == AdjList.end()) {
+        AdjList[website] = {}; // Initialize with an empty vector
+    }
+}
+
+void AdjacencyList::calcPowerRank(vector<double>& initialRank, int powerIterations) {
+    vector<double> newRank(initialRank.size(), 0.0);
+
+    // create a map that associates each website with its index in initialRank
+    map<string, int> websiteIndex;
+    int index = 0;
+    for (const auto& website : AdjList) {
+        websiteIndex[website.first] = index++;
+    }
+
+    // power iteration loop to continue multiplying until finished
+    for (int i = 1; i < powerIterations; i++) {
+        // reset temp vector newRank for storing new values
+        fill(newRank.begin(), newRank.end(), 0.0);
+        int counter = 0; // helps keep track of what value should be replaced in initialValue vector
+        for (auto &website : AdjList) {
+            double resultRank = 0;
+            for(auto &connections : website.second) {
+                int toIndex = websiteIndex[connections.first];
+                resultRank += (connections.second * initialRank[toIndex]);
+            }
+            newRank[counter] = resultRank;
+            counter++;
+        }
+        initialRank = newRank;
+    }
+    // round each value to two decimal places
+    for (double& rank : initialRank) {
+        rank = round(rank * 100) / 100.0;
+    }
+}
+
+void AdjacencyList::printPageRank(vector<double> initialRank) {
+    int counter = 0;
     string result;
+    // loop through each page in AdjList and print with precision
+    for (auto& website : AdjList) {
+        cout << website.first << " " << fixed << setprecision(2) << initialRank[counter++] << "\n";
+    }
+}
 
-    // do your page rank
+int main() {
+    // initialize variables & graph
+    int num_lines, power_iterations;
+    string from, to;
+    AdjacencyList adjList; // instance of class
+    set<string> websites;
 
-    cout << result;
-    return result;
+    // Take user input & create graph vertexes
+    cin >> num_lines;
+    cin >> power_iterations;
+    for (int i = 0; i < num_lines; i++) {
+        cin >> from;
+        cin >> to;
+        adjList.insertEdge(from, to, websites);
+    }
+
+    // ensure all websites in the set have an entry in adjacency list
+    for (const auto& website : websites) {
+        adjList.insertEmptyNode(website);
+    }
+
+    // create default ranks based off number of websites
+    int numWebsites = websites.size();
+    vector<double> initialRank(numWebsites, 1.0 / numWebsites);
+
+    // call functions to calculate the power rank & print the results
+    adjList.calcPowerRank(initialRank, power_iterations);
+    adjList.printPageRank(initialRank);
+    return 0;
 }
--- src/AdjacencyList.h
+++ src/AdjacencyList.h
@@ -1,15 +1,30 @@
 #pragma once
-
 #include <string>
-
+#include <map>
+#include <vector>
+#include <iterator>
+#include <set>
 using namespace std;
 
 class AdjacencyList {
-    private:
-    //Think about what member variables you need to initialize
-    public:
-    //Think about what helper functions you will need in the algorithm
-    string PageRank(int n);
+private:
+    // data structure to represent website adjacency list
+    map<string, vector<pair<string, double>>> AdjList;
+public:
+    // adds a new edge between from and to vertexes
+    void insertEdge(const string& from, const string& to, set<string>& websites);
+    // adds an empty weighted edge if called
+    void insertEmptyNode(const string& website);
+    // calculate the rank of the page with power iteration
+    void calcPowerRank(vector<double>& initialRank, int powerIterations);
+    // getter function for out-degrees
+    int getOutDegree(const string& website);
+    // getter function to allow finding size of AdjList in loops
+    const map<string, vector<pair<string, double>>>& getAdjList() const {
+        return AdjList;
+    }
+    // prints the PageRank of all pages after p powerIterations in ascending alphabetical order of webpages and
+    // rounding rank to two decimal places
+    void printPageRank(vector<double> initialRank);
 };
 
-// This class and method are optional.
